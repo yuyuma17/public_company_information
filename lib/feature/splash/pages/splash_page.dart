0c680aabef0801c3e80ad5b89826080ce795e638
Lacie <dark830917@gmail.com>
2022-11-29 21:04:35 +0800

    feat: add image and indicator on splash page

--- lib/feature/splash/pages/splash_page.dart
+++ lib/feature/splash/pages/splash_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class SplashPage extends StatelessWidget {
   static const routeName = '/splash';
@@ -7,7 +9,44 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              _SplashImage(),
+              Text('臺灣證券交易所'),
+              _UpdateIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SplashImage extends StatelessWidget {
+  const _SplashImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/zh/thumb/6/64/TWSE_logo.svg/250px-TWSE_logo.svg.png',
+      errorWidget: (_, __, ___) => const SizedBox.shrink(),
+    );
+  }
+}
+
+class _UpdateIndicator extends StatelessWidget {
+  const _UpdateIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 100.0),
+      child: CupertinoActivityIndicator(),
     );
   }
 }

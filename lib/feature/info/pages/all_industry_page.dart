eee522d1a96b61ed628d49231b07e49daa069563
Lacie <dark830917@gmail.com>
2022-12-01 00:26:15 +0800

    feat: show all industry list (data integration)

--- lib/feature/info/pages/all_industry_page.dart
+++ lib/feature/info/pages/all_industry_page.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/get_all_info_cubit.dart';
+import '../../../core/entities/enum/Industry.dart';
 
 class AllIndustryPage extends StatelessWidget {
   static const routeName = '/all-industry';
+
   const AllIndustryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('產業別')),
-      body: Container(),
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: const _IndustryList(),
+      ),
+    );
+  }
+}
+
+class _IndustryList extends StatelessWidget {
+  const _IndustryList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetAllInfoCubit, GetAllInfoState>(
+      builder: (context, state) {
+        return state.maybeWhen(
+          success: (industryGroupMap) {
+            return ListView.separated(
+              itemCount: industryGroupMap.length,
+              itemBuilder: (BuildContext context, int index) {
+                final entry = industryGroupMap.entries.elementAt(index);
+                final name = entry.key.chineseName;
+                final count = entry.value.length;
+                return GestureDetector(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 15.0,
+                    ),
+                    child: Text('$name($count)'),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
+            );
+          },
+          orElse: () => const SizedBox.shrink(),
+        );
+      },
     );
   }
 }
---
+++ lib/feature/info/pages/specific_industry_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpecificIndustryPage extends StatelessWidget {
+  static const routeName = '/specific-industry';
+
+  const SpecificIndustryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
